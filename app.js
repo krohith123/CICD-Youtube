09921f2bec8e4949203fc138cd11ad15b84674c0
krohith123 <90949720+krohith123@users.noreply.github.com>
2022-12-15 15:41:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 
-const express = require("express");
-require("dotenv").config();
-const app = express();
+// const express = require("express");
+// require("dotenv").config();
+// const app = express();
 
-// route
-app.get("/", (req, res) => {
-  // Sending This is the home page! in the page
-  res
-    .status(200)
-    .send(`This is the home page and running on ${process.env.PORT}`);
-});
+// // route
+// app.get("/", (req, res) => {
+//   // Sending This is the home page! in the page
+//   res
+//     .status(200)
+//     .send(`This is the home page and running on ${process.env.PORT}`);
+// });
 
-// Listening to the port 
-let PORT = process.env.PORT ? process.env.PORT : 8000;
-console.log(PORT);
-app.listen(PORT, () => {
-  console.log(`Server is running...`);
-});
+// // Listening to the port 
+// let PORT = process.env.PORT ? process.env.PORT : 8000;
+// console.log(PORT);
+// app.listen(PORT, () => {
+//   console.log(`Server is running...`);
+// });
+
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('A Monk in Cloud testing'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
